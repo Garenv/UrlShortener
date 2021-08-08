e55131bc9c729dd09d9cf8de8a9c382bd40b6eac
garenv <garenvartanian1992@gmail.com>
2021-08-08 09:44:28 -0400

    trying to figure out why $find->link is null

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\LinkShortenerController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,9 +12,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-//Route::get();
+Route::get('shortenedLink', [LinkShortenerController::class, 'index']);
+Route::post('shortenedLink', [LinkShortenerController::class, 'storeShortenedLink'])->name('shortenedLink');
+Route::get('{code}', [LinkShortenerController::class, 'getShortenedLink'])->name('goToShortenedLink');
